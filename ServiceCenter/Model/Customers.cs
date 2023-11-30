f4ae7d3a44d052c7977fc67dc576459ab8ee7af3
patrickbreids <patrick.breidsprecher@marquette.edu>
2023-11-30 15:14:04 -0600

    Create Customers class for authentication method

---
+++ ServiceCenter/Model/Customers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Model;
+
+internal class Customers
+{
+    public List<Customer> customers {  get; set; } //just a reference variable no list at this point
+
+    public Customers() 
+    { 
+        customers = new List<Customer>(); // now there is an actual list
+    }
+
+    public Customer Authenticate(string username, string password)
+    {
+        var c = customers.Where(o => (o.UserName == username) && (o.Password == password));
+
+        if(c.Count() > 0) 
+        { 
+            return c.First();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
