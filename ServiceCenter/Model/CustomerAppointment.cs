c881c32221b1e72cb3b980e9a0ce74197e2eb5d2
patrickbreids <patrick.breidsprecher@marquette.edu>
2023-11-30 15:00:32 -0600

    Customer Appointment info

---
+++ ServiceCenter/Model/CustomerAppointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.Model;
+
+internal class CustomerAppointment
+{
+    public Customer customer { get; set; }
+
+    public Appointment appointment { get; set; }
+
+    public CustomerAppointment(Customer c, Appointment a)
+    {
+        customer = c;
+        appointment = a;
+
+    }
+
+
+}
