d008879678f4783b0631754baa57b1d57f75681b
daniel-nava23 <daniel.nava@marquette.edu>
2023-12-01 01:17:31 -0600

    Create customer class.

---
+++ ServiceCenter/Model/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ServiceCenter.Model
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string UserName { get; set; }
+
+		public string Password { get; set; }
+	}
+}
+
