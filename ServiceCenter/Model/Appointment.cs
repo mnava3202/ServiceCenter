71e0f1a544ba269c543a316df87594c77465553c
daniel-nava23 <daniel.nava@marquette.edu>
2023-12-01 01:24:43 -0600

    Create Appointment and CustomerAppointment class.

---
+++ ServiceCenter/Model/Appointment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ServiceCenter.Model
+{
+	public class Appointment
+	{
+		public string Id { get; set; }
+
+		public string Notes { get; set; }
+
+		public DateTime dateTime { get; set; }
+	}
+}
+
---
+++ ServiceCenter/Model/CustomerAppointment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ServiceCenter.Model
+{
+	public class CustomerAppointment
+	{
+		public Customer customer { get; set; }
+
+		public Appointment appointment { get; set; }
+
+		public CustomerAppointment(Customer c, Appointment a)
+		{
+			customer = c;
+			appointment = a;
+		}
+	}
+}
+
